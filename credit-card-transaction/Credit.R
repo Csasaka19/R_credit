a370de6cccf6406882302ef6cf3e45c4fa7d6e62
Clive Sasaka <clivesasaka@gmail.com>
2023-11-18 13:08:00 +0300

    Add a gender pie chart and a transaction frequency line graph.Rubric updated

--- credit-card-transaction/Credit.R
+++ credit-card-transaction/Credit.R
@@ -1,10 +1,10 @@
 # Relevant libraries 
 library(dplyr)          # Data manipulation library
-library(ggplot2)        # Static visualization library
+library(ggplot2) # Static visualization library
 library(lubridate)      # Datetime library
 
 # Imports the data from a csv file
-credit <- read.csv("~/Ubuntu_projects/R/credit-card-transaction/credit_card_transaction_flow.csv")
+credit <- read.csv("~/R_credit/credit-card-transaction/credit_card_transaction_flow.csv")
 
 more_credit <- credit %>%
   select(Customer.ID, Birthdate, Date, Transaction.Amount, Category, Gender, Name, Surname, Merchant.Name) %>%
@@ -23,7 +23,7 @@ head(more_credit)
 
 # Plots
 
-
+# A histogram to track transaction amount and identify outliers
 hist(more_credit$Transaction.Amount,
      main="Histogram on transaction Amount",
      xlab="Transaction Amount in Dollars",
@@ -35,6 +35,7 @@ hist(more_credit$Transaction.Amount,
      breaks= 12
 )
 
+# Box plot for Category analysis
 boxplot(more_credit$Transaction.Amount ~ more_credit$Category,
         main = "Boxplot of Categories by Transaction",
         xlab = "Categories",
@@ -43,7 +44,30 @@ boxplot(more_credit$Transaction.Amount ~ more_credit$Category,
         col= "grey",
 )
 
-#Creating a Stacked bar plot
+# Creating a Stacked bar plot for category analysis
 ggplot(more_credit, aes(fill=Category, y=Transaction.Amount, x=Month)) + 
 geom_bar(stat='identity')
 
+
+# Create a pie chart on Gender
+gender_counts <- table(more_credit$Gender)
+colors <- c("green", "pink","skyblue")
+
+pie(gender_counts,
+    main = "Pie Chart of Gender Distribution",
+    labels = c("Not specified", "Female", "Male"),
+    edges = 400,
+    radius = 0.8,
+    col = colors,
+    border = "black",
+    clockwise = TRUE,
+    angle = 60
+)
+
+ggplot(more_credit, aes(x = paste(Year, Month, sep = "-"), group = 1)) +
+  geom_line(stat = "count", aes(y = after_stat(count), color = "blue")) +
+  scale_x_discrete(labels = scales::date_format("%Y-%m")) +
+  labs(title = "Line Plot of Transaction Frequency Over Time",
+       x = "Month and Year",
+       y = "Transaction Frequency") +
+  theme_minimal()
