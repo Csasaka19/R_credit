626bcfb6e2170e452dde31d786f5faf4e6e62f1a
Clive Sasaka <clivesasaka@gmail.com>
2023-11-24 11:11:50 +0300

    Add the datatable required for the heat map and the category factor

--- Credit-App/app.R
+++ Credit-App/app.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(plotly)
 
 # Imports the data from a csv file
-credit <- read.csv("~/R_credit/Credit_card_analysis_static/credit_card_transaction_flow.csv", stringsAsFactors = FALSE)
+credit <- read.csv("~/R_credit/Credit-App/credit_card_transaction_flow.csv", stringsAsFactors = FALSE)
 
 # Convert the 'Date' column to Date object
 credit$Date <- as.Date(credit$Date)
@@ -23,12 +23,18 @@ more_credit <- credit %>%
     Transaction_Category_Count = ave(Transaction.Amount, Category, FUN = length)
   )
 
+# New table for the the heatmap
+heatmap_data <- table(more_credit$Age, more_credit$Category)
+
+# Convert more_credit$Category to a factor
+more_credit$Category <- as.factor(more_credit$Category)
+
 # Define UI
 ui <- navbarPage(
   title = "Credit Card Transactions Analysis Dashboard",
   
   tabPanel(
-    "Plots",
+    "Plots View",
     sidebarLayout(
       sidebarPanel(
         h1("SideBar"),
